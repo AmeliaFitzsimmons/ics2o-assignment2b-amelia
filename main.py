782011b31f926ba4b998a17d4c78321db4228763
Amelia Fitzsimmons <113189695+AmeliaFitzsimmons@users.noreply.github.com>
2022-11-09 15:27:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Area And Circumferance of a Circle")
+d = float(input("Enter your Diameter:"))
+r = d/2
+c = 2*3.14*r
+ar = 3.14*r*r
+print("The circumference of your circle is:", c)
+print("The area of your circle is:", ar)
